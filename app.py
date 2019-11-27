18fd899eff278beb59ee1ee7f62d8e750a558bbc
shellkore <shellkoresahu@gmail.com>
2019-11-27 11:51:39 +0530

    created db in sqlite3

--- app.py
+++ app.py
@@ -9,5 +9,23 @@ app = Flask(__name__)
 def home():
    return ('Success') 
 
+@app.route('/host',methods = ['POST', 'GET'])
+def host():
+	if request.method == 'GET':
+		return render_template('host.html')
+	else:
+		name = request.form['name']
+		email = request.form['email']
+		phone = request.form['phone']
+		address = request.form['address']
+		print(type(name))
+		print(name)
+		print(email)
+		print(type(phone))
+		print(type(address))
+		msg = "read succesfully"
+		return render_template("result.html",msg = msg)
+
+
 if __name__ == '__main__':
    app.run(debug = True, port = 5000)
