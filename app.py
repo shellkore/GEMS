dbf16d4dc5720aa58f81fb02fe48bbac2f5f7350
shellkore <shellkoresahu@gmail.com>
2019-11-27 12:08:12 +0530

    created view for host table

--- app.py
+++ app.py
@@ -17,15 +17,31 @@ def host():
 		name = request.form['name']
 		email = request.form['email']
 		phone = request.form['phone']
+		phone = int(phone)
 		address = request.form['address']
-		print(type(name))
-		print(name)
-		print(email)
-		print(type(phone))
-		print(type(address))
-		msg = "read succesfully"
+		with sql.connect("database.db") as con:
+			cur = con.cursor()
+
+			cur.execute("INSERT INTO host (name,email,phone,address) VALUES (?,?,?,?)",(name,email,phone,address))
+
+			con.commit()
+			cur.close()
+			msg = "Record successfully added"
 		return render_template("result.html",msg = msg)
 
+@app.route('/viewH')
+def list():
+   con = sql.connect("database.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from host")
+   
+   rows = cur.fetchall()
+   for row in rows:
+   	print(row['name'])
+   	
+   return render_template("viewH.html",rows = rows)
 
 if __name__ == '__main__':
    app.run(debug = True, port = 5000)
